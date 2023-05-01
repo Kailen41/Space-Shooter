25b0eae19ab6b7934f1ecef6a20455a2b912c86c
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2023-05-01 02:14:24 -0400

    Player creation and Movement Functionality
    -Sample Scene
    +Game Scene
    +Player primitive material
    +Player script that controls movement of the player with WASD, using horizontal and vertical input commands.
    Player script also constrains the player on the Y axis, using MathF Clamp. Min is -4 and max is 0.
    Player script also wraps the player on the X axis. When the player moves to the right and hits 11.5f, it sets the player position to -11.5 and vise versa

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.5f;
+
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+
+        PlayerBoundsOnXAndYAxis();
+    }
+
+    private void PlayerMovement()
+    {
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        float _verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 _direction = new Vector3(_horizontalInput, _verticalInput);
+
+        transform.Translate(_direction * _speed * Time.deltaTime);
+    }
+
+    private void PlayerBoundsOnXAndYAxis()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.0f, 0), 0);
+
+        if (transform.position.x >= 11.5f)
+        {
+            transform.position = new Vector3(-11.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.5f)
+        {
+            transform.position = new Vector3(11.5f, transform.position.y, 0);
+        }
+    }
+
+} // End Class
